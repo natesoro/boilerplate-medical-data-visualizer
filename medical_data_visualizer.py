867039cabd1df77c5db0f42d6243088d9365151e
niño tesoro <na.tesoro@gmail.com>
2024-06-21 05:17:53 +0000

    updates to the implementations

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -20,34 +20,16 @@ df['overweight'] = df['bmi'].apply(lambda x: 1 if x > 25.0 else 0)
 normalizer = np.vectorize(normalize_data)
 df['gluc'] = df.apply(lambda v: normalizer(v['gluc']), axis=1)
 df['cholesterol'] = df.apply(lambda v: normalizer(v['cholesterol']), axis=1)
+df=df.astype({'overweight':int, 'cholesterol':int, 'gluc':int})#normalize datatypes for these columns so that they are treated the same during grouping
 
 # 4
 def draw_cat_plot():
     # 5
     df_cat = df.melt(id_vars=['cardio'],value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
-
     # 6
-    df_cat.rename(columns={'value':'total'}, inplace=True)
-    #cardio 1, value 1
-    df_cat1=df_cat[(df_cat['cardio']==1) & (df_cat['total']==1)].groupby(['variable']).count().reset_index()
-    df_cat1["cardio"]=1
-    df_cat1["value"]=1
-    #cardio 1, value 0
-    df_cat2=df_cat[(df_cat['cardio']==1) & (df_cat['total']==0)].groupby(['variable']).count().reset_index()
-    df_cat2["cardio"]=1
-    df_cat2["value"]=0
-    #cardio 0, value=1
-    df_cat3=df_cat[(df_cat['cardio']==0) & (df_cat['total']==1)].groupby(['variable']).count().reset_index()
-    df_cat3["cardio"]=0
-    df_cat3["value"]=1
-    #cardio 0, value=0
-    df_cat4=df_cat[(df_cat['cardio']==0) & (df_cat['total']==0)].groupby(['variable']).count().reset_index()
-    df_cat4["cardio"]=0
-    df_cat4["value"]=0
-    #combine to finalize
-    df_cat=pd.concat([df_cat1, df_cat2, df_cat3, df_cat4])
-
+    df_cat['total']=1
+    df_cat=df_cat.groupby(['cardio', 'variable', 'value'])['total'].sum().reset_index()
 
     # 7
     plot = sns.catplot(
@@ -61,7 +43,7 @@ def draw_cat_plot():
 
 
     # 9
-    fig.savefig('catplot2.png')
+    fig.savefig('catplot.png')
     return fig
 
 
@@ -75,13 +57,6 @@ def draw_heat_map():
     df_heat.rename(columns={'sex':'gender'}, inplace=True)
     df_heat = df_heat.drop('bmi', axis=1)
 
-    """df_heat = df[(df['height']>=df['height'].quantile(0.025))]
-    df_heat = df_heat[(df_heat['height']>=df_heat['height'].quantile(0.975))]
-    df_heat = df_heat[(df_heat['weight']>=df_heat['weight'].quantile(0.025))]
-    df_heat = df_heat[(df_heat['weight']<=df_heat['weight'].quantile(0.975))]
-    df_heat.reset_index(drop=True)"""
-
-
     # 12
     corr = df_heat.corr()
 
@@ -89,13 +64,14 @@ def draw_heat_map():
     mask = np.triu(np.ones_like(corr))
 
     # 14
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(9,6))
 
 
     # 15
-    fig=sns.heatmap(corr, mask=mask, annot=True, fmt=".1f")
-    #fig=fig.figure
+    fig=sns.heatmap(corr, mask=mask,annot=True, fmt=".1f",vmin=corr.min().min(), vmax=.3, 
+                cbar_kws={'shrink': 0.5, 'ticks':[.24,.16,.08,.00,-0.08]}, ax=ax, center=0).figure
+
 
     # 16
-    fig.figure.savefig('heatmap.png')
+    fig.savefig('heatmap.png')
     return fig
